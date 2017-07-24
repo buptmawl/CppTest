ed9a0b2e4c4b8280154850d260ca86070226f03d
mawenlong <mawenlong@bytedance.com>
2017-07-24 12:10:17 +0800

    add combination && find upper_bound number
    Change-Id: Id6ff7abf886e54208e65c7d8ebcd47ecb519ac19

--- c++11/combination.cc
+++ c++11/combination.cc
@@ -10,19 +10,12 @@ using namespace std;
 // clang++ -std=c++11 -stdlib=libc++ median_nums_two_sorted.cc && ./a.out
 
 
-void dfs(vector<int> & input, int indx, int len, vector<int> & ret_tmp, vector<vector<int>> &ret) {
-
+void dfs(vector<int> & input, int len, int index, vector<int> & ret_tmp, vector<vector<int>> &ret) {
     ret.push_back(ret_tmp);
-    //if(ret_tmp.size() == len ) {
-    //    //cout << "result:";
-    //    //for (auto &n : ret_tmp) {
-    //    //    cout << " " << n;
-    //    //}
-    //    //cout << endl;
-    //}
-    for(int k=indx; k<len; k++) {
+
+    for(int k=index; k<len; k++) {
         ret_tmp.push_back(input[k]); 
-        dfs(input, k+1, len, ret_tmp, ret);
+        dfs(input, len, k+1, ret_tmp, ret);
         ret_tmp.pop_back();
     }
 }
@@ -32,21 +25,40 @@ vector<vector<int>> resolve(vector<int> & input) {
     vector<vector<int>> ret;
     vector<int> tmp;
 
-    dfs(input, 0, input.size(), tmp, ret);
+    dfs(input, input.size(), 0, tmp, ret);
     return ret;
 }
 
 int main() {
     vector<int> vec = {1, 2, 3};
+    sort(vec.begin(), vec.end());
 
     vector<vector<int>> ret = resolve(vec);
 
+    vector<int> result_vec;
     for(auto &m : ret) {
+        int kk=0;
+        cout << " " ;
         for (auto &n : m) {
+            kk = 10*kk+n;
             cout << " " << n;
         }
         cout << endl;
+        result_vec.push_back(kk);
     }
+
+    sort(result_vec.begin(), result_vec.end());
+    for (auto &m : result_vec) {
+        cout << " " << m ; 
+    }
+
+    cout << "please input a M: " << endl;
+
+    string mstr;
+
+    cin >> mstr;
+
+    cout << (std::upper_bound(result_vec.begin(), result_vec.end(), stoi(mstr))-result_vec.begin())  << endl;
 
     return 0;
 }
